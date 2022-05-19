7d7dadaa28cadc6043a7a39d4a7b5f51913e47fa
XIAOSENNIN <101668502+XIAOSENNIN@users.noreply.github.com>
2022-05-19 18:34:38 -0400

    Add files via upload

---
+++ BD_test/BD_test/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace BD_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_patente = new System.Windows.Forms.TextBox();
+            this.txt_marca = new System.Windows.Forms.TextBox();
+            this.txt_modelo = new System.Windows.Forms.TextBox();
+            this.btn_insertar = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_modificar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Marca";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Modelo";
+            // 
+            // txt_patente
+            // 
+            this.txt_patente.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_patente.Location = new System.Drawing.Point(111, 20);
+            this.txt_patente.MaxLength = 6;
+            this.txt_patente.Name = "txt_patente";
+            this.txt_patente.Size = new System.Drawing.Size(179, 20);
+            this.txt_patente.TabIndex = 3;
+            // 
+            // txt_marca
+            // 
+            this.txt_marca.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_marca.Location = new System.Drawing.Point(111, 45);
+            this.txt_marca.MaxLength = 10;
+            this.txt_marca.Name = "txt_marca";
+            this.txt_marca.Size = new System.Drawing.Size(179, 20);
+            this.txt_marca.TabIndex = 4;
+            // 
+            // txt_modelo
+            // 
+            this.txt_modelo.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txt_modelo.Location = new System.Drawing.Point(111, 74);
+            this.txt_modelo.MaxLength = 10;
+            this.txt_modelo.Name = "txt_modelo";
+            this.txt_modelo.Size = new System.Drawing.Size(179, 20);
+            this.txt_modelo.TabIndex = 5;
+            // 
+            // btn_insertar
+            // 
+            this.btn_insertar.Location = new System.Drawing.Point(43, 231);
+            this.btn_insertar.Name = "btn_insertar";
+            this.btn_insertar.Size = new System.Drawing.Size(75, 23);
+            this.btn_insertar.TabIndex = 6;
+            this.btn_insertar.Text = "Insertar";
+            this.btn_insertar.UseVisualStyleBackColor = true;
+            this.btn_insertar.Click += new System.EventHandler(this.btn_insertar_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 114);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(341, 89);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(146, 231);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_eliminar.TabIndex = 8;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.Location = new System.Drawing.Point(247, 231);
+            this.btn_modificar.Name = "btn_modificar";
+            this.btn_modificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_modificar.TabIndex = 9;
+            this.btn_modificar.Text = "Modificar";
+            this.btn_modificar.UseVisualStyleBackColor = true;
+            this.btn_modificar.Click += new System.EventHandler(this.btn_modificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 289);
+            this.Controls.Add(this.btn_modificar);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_insertar);
+            this.Controls.Add(this.txt_modelo);
+            this.Controls.Add(this.txt_marca);
+            this.Controls.Add(this.txt_patente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_patente;
+        private System.Windows.Forms.TextBox txt_marca;
+        private System.Windows.Forms.TextBox txt_modelo;
+        private System.Windows.Forms.Button btn_insertar;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_modificar;
+    }
+}
+
---
+++ BD_test/BD_test/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BD_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_insertar_Click(object sender, EventArgs e)
+        {
+            if (txt_patente.Text != "" && txt_marca.Text != "" && txt_modelo.Text != "")
+            {
+                Automovil auto = new Automovil();
+
+                auto.patente = txt_patente.Text;
+                auto.marca = txt_marca.Text;
+                auto.modelo = txt_modelo.Text;  
+
+                ProgCEntities bd = new ProgCEntities();
+
+                bd.Automovil.Add(auto);
+                bd.SaveChanges();
+                mostrarAutomovil();
+
+                txt_patente.Clear();
+                txt_marca.Clear();
+                txt_modelo.Clear(); 
+
+                MessageBox.Show("Los datos se han guardado correctamente",
+                    "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Debe ingresar todos los datos para guardar",
+                    "Guardar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mostrarAutomovil();
+        }
+
+        private void mostrarAutomovil()
+        {
+            ProgCEntities bd = new ProgCEntities();
+
+            var miConsulta = from mi_item in bd.Automovil
+                             select mi_item;
+
+            dataGridView1.DataSource = miConsulta.ToList();
+            dataGridView1.Refresh();
+        }
+
+        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewRow Fila = dataGridView1.Rows[e.RowIndex];
+
+            txt_patente.Text = Fila.Cells[0].Value.ToString();
+            txt_marca.Text = Fila.Cells[1].Value.ToString();
+            txt_modelo.Text = Fila.Cells[2].Value.ToString();
+
+            btn_insertar.Enabled = false;
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            ProgCEntities bd = new ProgCEntities();
+
+            var miConsulta = bd.Automovil.Find(txt_patente.Text);
+
+            if (miConsulta != null)
+            {
+                bd.Automovil.Remove(miConsulta);
+                bd.SaveChanges();
+                mostrarAutomovil();
+
+                txt_patente.Clear();
+                txt_marca.Clear();
+                txt_modelo.Clear();
+
+                MessageBox.Show("Los datos se eliminaron correctamente",
+                   "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                btn_insertar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fila para eliminar los datos",
+                   "Eliminar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btn_modificar_Click(object sender, EventArgs e)
+        {
+            ProgCEntities bd = new ProgCEntities();
+
+            var miConsulta = bd.Automovil.Find(txt_patente.Text);
+
+            if (miConsulta != null)
+            {
+                miConsulta.patente = txt_patente.Text;
+                miConsulta.marca = txt_marca.Text;
+                miConsulta.modelo = txt_modelo.Text;
+
+                bd.SaveChanges();
+                mostrarAutomovil();
+
+                txt_patente.Clear();
+                txt_marca.Clear();
+                txt_modelo.Clear();
+
+                MessageBox.Show("Los datos se han modificados correctamente",
+                   "Modificar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                btn_insertar.Enabled = true;
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fila para modificar",
+                   "Modificar", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+        }
+    }
+}
